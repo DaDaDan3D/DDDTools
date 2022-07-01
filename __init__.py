be4095401d1eb577783ae5f3c08485876ad70341
DaDaDan3D <108515309+DaDaDan3D@users.noreply.github.com>
2022-07-01 23:09:33 +0900

    Update __init__.py
    Version 0.1.1
    
    Maintain module reloading area.

--- __init__.py
+++ __init__.py
@@ -2,11 +2,9 @@
 Copyright (c) 2022 DaDaDan3D
 Released under the MIT license
 https://opensource.org/licenses/mit-license.php
-
 """
 
 import sys, importlib
-import bpy
 
 bl_info = {
     "name": "DDDTools",
@@ -14,7 +12,7 @@ bl_info = {
     "warning": "Not much error handling. Use at your own risk.",
     "support": "TESTING",
     "author": "DaDaDan3D",
-    "version": (0, 1),
+    "version": (0, 1, 1),
     "blender": (3, 1, 0),
     "doc_url": "https://github.com/DaDaDan3D/DDDTools",
     "tracker_url": "https://github.com/DaDaDan3D/DDDTools/issues",
@@ -22,7 +20,7 @@ bl_info = {
     "location": "View3D > Sidebar > DDDTools"
 }
 
-module_names = [
+modules = [
     'internalUtils',
     'fitting',
     'BoneTool',
@@ -39,36 +37,27 @@ module_names = [
     'UI_WeightTool',
     'CalcSphereFromSelectedVertices',
 ]
-
-def import_or_reload_modules(module_names, package_name):
-    ensure_starts_with = lambda s, prefix: s if s.startswith(prefix) else prefix + s
-    module_names = [ensure_starts_with(name, f'{package_name}.') for name in module_names]
-    modules = []
-    for module_name in module_names:
-        module = sys.modules.get(module_name)
-        if module:
-            module = importlib.reload(module)
-        else:
-            module = globals()[module_name] = importlib.import_module(module_name)
-        modules.append(module)
-    return modules
+modules = [f'{__package__}.{mn}' for mn in modules]
 
 def reloadModules():
     unregister()
     register()
 
 def register():
-    import_or_reload_modules(module_names, __name__)
+    for mn in modules:
+        module = sys.modules.get(mn)
+        if module:
+            module = importlib.reload(module)
+        else:
+            module = importlib.import_module(mn)
 
-    for module_name in module_names:
-        module = globals()[module_name]
         if 'registerClass' in dir(module):
             module.registerClass()
 
 def unregister():
-    for module_name in reversed(module_names):
-        module = globals()[module_name]
-        if 'unregisterClass' in dir(module):
+    for mn in reversed(modules):
+        module = sys.modules.get(mn)
+        if module and 'unregisterClass' in dir(module):
             module.unregisterClass()
 
 if __name__ == '__main__':
