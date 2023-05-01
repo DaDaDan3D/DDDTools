6554803111c48e00cd5679f2c2cf6fc1916c2c34
DaDaDan3D <dadadan@cat.nifty.jp>
2023-05-02 08:32:48 +0900

    UI の修正
    ・オペレーターにプロパティを持たせてコードを整理
    ・UV をオフセット移動させる UI を追加

--- UI_UVTool.py
+++ UI_UVTool.py
@@ -1,47 +1,37 @@
 import bpy
-from bpy.props import FloatProperty, PointerProperty
+from bpy.props import FloatProperty, PointerProperty, FloatVectorProperty, EnumProperty
 from bpy.types import Panel, Operator, PropertyGroup
 
 from . import UVTool as ut
 
 ################################################################
-def getOriginX(self):
-    return bpy.context.space_data.cursor_location[0]
+def getOrigin(self):
+    return bpy.context.space_data.cursor_location
 
-def setOriginX(self, value):
-    bpy.context.space_data.cursor_location[0] = value
-
-def getOriginY(self):
-    return bpy.context.space_data.cursor_location[1]
-
-def setOriginY(self, value):
-    bpy.context.space_data.cursor_location[1] = value
+def setOrigin(self, value):
+    bpy.context.space_data.cursor_location = value
 
 ################
 class DDDUT_propertyGroup(PropertyGroup):
-    originX: FloatProperty(
-        name='2DカーソルX',
-        description='2DカーソルのX座標',
-        default=0.5,
-        min=0.00,
-        max=1.00,
+    origin: FloatVectorProperty(
+        name='2Dカーソル座標',
+        description='2Dカーソルの座標です',
+        size=2,
         precision=2,
         step=1.0,
         unit='NONE',
-        get=getOriginX,
-        set=setOriginX,
+        get=getOrigin,
+        set=setOrigin,
     )
-    originY: FloatProperty(
-        name='2DカーソルY',
-        description='2DカーソルのY座標',
-        default=0.5,
-        min=0.00,
-        max=1.00,
+    
+    offset: FloatVectorProperty(
+        name='移動量',
+        description='UV 座標に加える量です',
+        size=2,
+        default=[0.25, 0.25],
         precision=2,
         step=1.0,
         unit='NONE',
-        get=getOriginY,
-        set=setOriginY,
     )
     
 ################################################################
@@ -51,11 +41,27 @@ def canEditMesh(context):
         context.active_object.mode == 'EDIT'
 
 ################
-class DDDUT_OT_alignUVLeft(Operator):
-    bl_idname = 'dddut.align_uv_left'
-    bl_label = '→'
-    bl_description = 'UVの左端が原点になるように移動します'
+class DDDUT_OT_alignUV(Operator):
+    bl_idname = 'dddut.align_uv'
+    bl_label = 'UV 整列'
+    bl_description = '様々な条件で UV を整列させます'
     bl_options = {'UNDO'}
+
+    origin: FloatVectorProperty(
+        name='基点',
+        description='整列の基点となる座標です',
+        size=2,
+        precision=2,
+        step=1.0,
+        unit='NONE',
+    )
+    mode: EnumProperty(
+        items=[('ALIGN_LEFT', 'ALIGN_LEFT', 'UVの左端を基点に付けます'),
+               ('ALIGN_RIGHT', 'ALIGN_RIGHT', 'UVの右端を基点に付けます'),
+               ('ALIGN_TOP', 'ALIGN_TOP', 'UVの上端を基点に付けます'),
+               ('ALIGN_BOTTOM', 'ALIGN_BOTTOM', 'UVの下端を基点に付けます'),
+               ('ALIGN_CENTER_HORIZONTAL', 'ALIGN_CENTER_HORIZONTAL', 'UVの左右の中心を基点に付けます'),
+               ('ALIGN_CENTER_VERTICAL', 'ALIGN_CENTER_VERTICAL', 'UVの上下の中心を基点に付けます')])
 
     @classmethod
     def poll(self, context):
@@ -63,27 +69,23 @@ class DDDUT_OT_alignUVLeft(Operator):
 
     def execute(self, context):
         prop = context.scene.dddtools_ut_prop
-        return ut.offsetSelectedUVIsland('ALIGN_LEFT', prop.originX, 0)
+        return ut.offsetSelectedUVIsland(self.mode, self.origin[0], self.origin[1])
 
-class DDDUT_OT_alignUVRight(Operator):
-    bl_idname = 'dddut.align_uv_right'
-    bl_label = '←'
-    bl_description = 'UVの右端が原点になるように移動します'
-    bl_options = {'UNDO'}
+################
+class DDDUT_OT_moveUV(Operator):
+    bl_idname = 'dddut.move_uv'
+    bl_label = 'UV 移動'
+    bl_description = 'UV を移動させます'
+    bl_options = {'REGISTER', 'UNDO'}
 
-    @classmethod
-    def poll(self, context):
-        return canEditMesh(context)
-
-    def execute(self, context):
-        prop = context.scene.dddtools_ut_prop
-        return ut.offsetSelectedUVIsland('ALIGN_RIGHT', prop.originX, 0)
-
-class DDDUT_OT_alignUVTop(Operator):
-    bl_idname = 'dddut.align_uv_top'
-    bl_label = '↓'
-    bl_description = 'UVの上端が原点になるように移動します'
-    bl_options = {'UNDO'}
+    offset: FloatVectorProperty(
+        name='移動量',
+        description='UV 座標に加える量です',
+        size=2,
+        precision=2,
+        step=1.0,
+        unit='NONE',
+    )
 
     @classmethod
     def poll(self, context):
@@ -91,49 +93,7 @@ class DDDUT_OT_alignUVTop(Operator):
 
     def execute(self, context):
         prop = context.scene.dddtools_ut_prop
-        return ut.offsetSelectedUVIsland('ALIGN_TOP', 0, prop.originY)
-
-class DDDUT_OT_alignUVBottom(Operator):
-    bl_idname = 'dddut.align_uv_bottom'
-    bl_label = '↑'
-    bl_description = 'UVの下端が原点になるように移動します'
-    bl_options = {'UNDO'}
-
-    @classmethod
-    def poll(self, context):
-        return canEditMesh(context)
-
-    def execute(self, context):
-        prop = context.scene.dddtools_ut_prop
-        return ut.offsetSelectedUVIsland('ALIGN_BOTTOM', 0, prop.originY)
-
-class DDDUT_OT_alignUVCenterHorizontal(Operator):
-    bl_idname = 'dddut.align_uv_center_horizontal'
-    bl_label = '←→中央揃え'
-    bl_description = 'UVの左右の中心が原点になるように移動します'
-    bl_options = {'UNDO'}
-
-    @classmethod
-    def poll(self, context):
-        return canEditMesh(context)
-
-    def execute(self, context):
-        prop = context.scene.dddtools_ut_prop
-        return ut.offsetSelectedUVIsland('ALIGN_CENTER_HORIZONTAL', prop.originX, 0)
-
-class DDDUT_OT_alignUVCenterVertical(Operator):
-    bl_idname = 'dddut.align_uv_center_vertical'
-    bl_label = '↑↓中央揃え'
-    bl_description = 'UVの上下の中心が原点になるように移動します'
-    bl_options = {'UNDO'}
-
-    @classmethod
-    def poll(self, context):
-        return canEditMesh(context)
-
-    def execute(self, context):
-        prop = context.scene.dddtools_ut_prop
-        return ut.offsetSelectedUVIsland('ALIGN_CENTER_VERTICAL', 0, prop.originY)
+        return ut.offsetSelectedUVIsland('OFFSET', self.offset[0], self.offset[1])
 
 ################################################################
 class DDDUT_PT_UVTool(Panel):
@@ -147,38 +107,52 @@ class DDDUT_PT_UVTool(Panel):
         prop = context.scene.dddtools_ut_prop
         layout = self.layout.column()
 
+        def drawAlignUV(lo, text='', icon='NONE', mode=None):
+            op = lo.operator(DDDUT_OT_alignUV.bl_idname, text=text, icon=icon)
+            op.mode = mode
+            op.origin = prop.origin
+
         box = layout.box()
         col = box.column(align=True)
-        col.operator(DDDUT_OT_alignUVBottom.bl_idname,
-                     text='', icon='ANCHOR_BOTTOM')
+        col.label(text='整列')
+        drawAlignUV(col, icon='ANCHOR_BOTTOM', mode='ALIGN_BOTTOM')
         row = col.row(align=True)
         row.scale_y = 2.0
-        row.operator(DDDUT_OT_alignUVRight.bl_idname,
-                     text='', icon='ANCHOR_RIGHT')
+        drawAlignUV(row, icon='ANCHOR_RIGHT', mode='ALIGN_RIGHT')
         col_xy = row.column(align=True)
         col_xy.scale_y = 0.5
-        col_xy.prop(prop, 'originX', text='X')
-        col_xy.prop(prop, 'originY', text='Y')
-        row.operator(DDDUT_OT_alignUVLeft.bl_idname,
-                     text='', icon='ANCHOR_LEFT')
-        col.operator(DDDUT_OT_alignUVTop.bl_idname,
-                     text='', icon='ANCHOR_TOP')
-
-
+        col_xy.prop(prop, 'origin', text='')
+        drawAlignUV(row, icon='ANCHOR_LEFT', mode='ALIGN_LEFT')
+        drawAlignUV(col, icon='ANCHOR_TOP', mode='ALIGN_TOP')
+        
         col.separator()
-        col.operator(DDDUT_OT_alignUVCenterHorizontal.bl_idname)
-        col.operator(DDDUT_OT_alignUVCenterVertical.bl_idname)
+        drawAlignUV(col, text='←→ 水平揃え', mode='ALIGN_CENTER_HORIZONTAL')
+        drawAlignUV(col, text='↑↓ 垂直揃え', mode='ALIGN_CENTER_VERTICAL')
 
+        def drawMoveUV(lo, offsetX, offsetY, icon='NONE'):
+            op = lo.operator(DDDUT_OT_moveUV.bl_idname, text='', icon=icon)
+            op.offset = [offsetX, offsetY]
+
+        box = layout.box()
+        col = box.column(align=True)
+        col.label(text='移動')
+        drawMoveUV(col, 0, prop.offset[1], icon='TRIA_UP')
+        row = col.row(align=True)
+        row.scale_y = 2.0
+        drawMoveUV(row, -prop.offset[0], 0, icon='TRIA_LEFT')
+        col_xy = row.column(align=True)
+        col_xy.scale_y = 0.5
+        col_xy.prop(prop, 'offset', text='')
+        drawMoveUV(row, prop.offset[0], 0, icon='TRIA_RIGHT')
+        drawMoveUV(col, 0, -prop.offset[1], icon='TRIA_DOWN')
+        
+        
 
 ################################################################
 classes = (
     DDDUT_propertyGroup,
-    DDDUT_OT_alignUVLeft,
-    DDDUT_OT_alignUVRight,
-    DDDUT_OT_alignUVTop,
-    DDDUT_OT_alignUVBottom,
-    DDDUT_OT_alignUVCenterHorizontal,
-    DDDUT_OT_alignUVCenterVertical,
+    DDDUT_OT_alignUV,
+    DDDUT_OT_moveUV,
     DDDUT_PT_UVTool,
 )
 
