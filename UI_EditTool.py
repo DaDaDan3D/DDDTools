bb13691d022fd0163cbdc769d9300c28d8dc563a
DaDaDan3D <dadadan@cat.nifty.jp>
2023-05-14 20:56:52 +0900

    編集関連のツールをまとめ、UI_EditTool とする
    ・UI_SelectTool を UI_EditTool にリネーム
    ・CalcSphereFromSelectedVertices から UI_EditTool に移動
    ・convertEmptyAndSphere を UI_EditTool に追加
    ・バージョンを 0.4.4 に上げる

--- UI_EditTool.py
+++ UI_EditTool.py
@@ -1,14 +1,20 @@
 import bpy
+import bmesh
 from bpy.props import BoolProperty, FloatProperty, IntProperty
 from bpy.types import Panel, Operator
 
 import math
 from random import Random
+from mathutils import Vector, Matrix
+import traceback
+import numpy as np
+from . import internalUtils as iu
+from . import mathUtils as mu
 from . import SelectTool as st
 
 ################################################################
-class DDDST_OT_selectDividingLoops(Operator):
-    bl_idname = 'dddst.select_dividing_loops'
+class DDDET_OT_selectDividingLoops(Operator):
+    bl_idname = 'dddet.select_dividing_loops'
     bl_label = '分割線の選択'
     bl_description = '選択メッシュの分割線を選択します'
     bl_options = {'REGISTER', 'UNDO'}
@@ -221,27 +227,174 @@ class DDDST_OT_selectDividingLoops(Operator):
             return {'CANCELLED'}
 
 ################################################################
-class DDDST_PT_SelectTool(Panel):
-    bl_idname = 'ST_PT_WeightTool'
-    bl_label = 'SelectTool'
+def calcCenterOfSphereFromSelectedVertices():
+    """
+    Returns the radius and center of the sphere that approximates the selected vertices.
+    
+    """
+
+    mesh = bpy.context.active_object
+    if not mesh or mesh.type != 'MESH':
+        return None
+
+    bme = bmesh.from_edit_mesh(bpy.context.active_object.data)
+    mtx = bpy.context.active_object.matrix_world
+    verts = [mtx @ vtx.co for vtx in bme.verts if vtx.select]
+    #print(verts)
+    
+    try:
+        if len(verts) < 4:
+            raise ValueError(f'4つ以上の頂点を選択してください。現在{len(verts)}個の頂点が選択されています。')
+        elif len(verts) == 4:
+            result = mu.calcCircumcenter(np.array(verts))
+        else:
+            result = mu.calcFit(np.array(verts))
+    except Exception as e:
+        print(f'エラーが発生しました: {e} {type(e)}')
+        traceback.print_exc()
+        result = None
+
+    return result
+    
+class DDDET_OT_addApproximateSphere(Operator):
+    bl_idname = 'dddet.add_approximate_sphere'
+    bl_label = '近似球の追加'
+    bl_description = '選択した頂点群に近似した球を追加します'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    segments: IntProperty(
+        name='セグメント',
+        description='追加する球のセグメントの数を指定します',
+        min=3,
+        max=500,
+        default=32,
+    )
+
+    ring_count: IntProperty(
+        name='リング',
+        description='追加する球のリングの数を指定します',
+        min=3,
+        max=500,
+        default=16,
+    )
+
+    @classmethod
+    def poll(self, context):
+        mesh = bpy.context.active_object
+        return mesh and mesh.type=='MESH' and mesh.mode=='EDIT'
+
+    def execute(self, context):
+        if self.sphere:
+            mesh = iu.ObjectWrapper(bpy.context.active_object)
+            modeChanger = iu.ModeChanger(mesh.obj, 'OBJECT')
+            bpy.ops.mesh.primitive_uv_sphere_add(enter_editmode=False,
+                                                 align='WORLD',
+                                                 segments=self.segments,
+                                                 ring_count=self.ring_count,
+                                                 radius=self.sphere[0],
+                                                 location=self.sphere[1],
+                                                 scale=(1, 1, 1))
+            del modeChanger
+            bpy.context.view_layer.objects.active = mesh.obj
+            return {'FINISHED'}
+        else:
+            self.report({'WARNING'},
+                        '球を追加できませんでした。詳細はログを参照してください')
+            return {'CANCELLED'}
+
+    def invoke(self, context, event):
+        self.sphere = calcCenterOfSphereFromSelectedVertices()
+        return self.execute(context)
+
+################################################################
+class DDDET_OT_addApproximateEmpty(Operator):
+    bl_idname = 'dddet.add_approximate_empty'
+    bl_label = 'エンプティ球の追加'
+    bl_description = '選択した頂点群に近似したEMPTY球を追加します'
+    bl_options = {'UNDO'}
+
+    @classmethod
+    def poll(self, context):
+        mesh = bpy.context.active_object
+        return mesh and mesh.type=='MESH' and mesh.mode=='EDIT'
+
+    def execute(self, context):
+        sphere = calcCenterOfSphereFromSelectedVertices()
+        if sphere:
+            mesh = iu.ObjectWrapper(bpy.context.active_object)
+            empty_obj = bpy.data.objects.new(f'EmptySphere_{mesh.name}', None)
+            empty_obj.empty_display_type = 'SPHERE'
+            empty_obj.empty_display_size = sphere[0]
+            empty_obj.location = sphere[1]
+            context.scene.collection.objects.link(empty_obj)
+            return {'FINISHED'}
+        else:
+            self.report({'WARNING'},
+                        'エンプティ球を追加できませんでした。詳細はログを参照してください')
+            return {'CANCELLED'}
+
+################################################################
+class DDDET_OT_convertEmptyAndSphere(Operator):
+    bl_idname = 'dddet.convert_empty_and_sphere'
+    bl_label = 'エンプティとメッシュの相互変換'
+    bl_description = '選択中のオブジェクトがメッシュならエンプティ球に、エンプティ球ならメッシュに変換します'
+    bl_options = {'UNDO'}
+
+    @classmethod
+    def poll(self, context):
+        return bpy.context.selected_objects
+
+    def execute(self, context):
+        emptyObjects = []
+        sphereObjects = []
+        for obj in bpy.context.selected_objects:
+            if obj.type == 'MESH':
+                empty = iu.convertSphereToEmpty(obj)
+                if empty:
+                    emptyObjects.append(empty)
+            elif obj.type == 'EMPTY' and obj.empty_display_type == 'SPHERE':
+                sphere = iu.convertEmptyToSphere(obj)
+                if sphere:
+                    sphereObjects.append(sphere)
+        if sphereObjects or emptyObjects:
+            self.report({'INFO'},
+                        f'{len(sphereObjects)} 個のエンプティと {len(emptyObjects)} 個のメッシュを変換しました: {sphereObjects} {emptyObjects}')
+            for obj in sphereObjects + emptyObjects:
+                obj.select_set(True)
+            return {'FINISHED'}
+        else:
+            self.report({'INFO'},
+                        '変換できるオブジェクトはありませんでした')
+            return {'CANCELLED'}
+
+################################################################
+class DDDET_PT_main(Panel):
+    bl_idname = 'DDDET_PT_main'
+    bl_label = 'EditTool'
     bl_category = "DDDTools"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
 
     def draw(self, context):
-        layout = self.layout
-        layout.operator(DDDST_OT_selectDividingLoops.bl_idname)
-    
+        col = self.layout.column()
+        col.operator(DDDET_OT_selectDividingLoops.bl_idname)
+        col.operator(DDDET_OT_addApproximateSphere.bl_idname)
+        col.operator(DDDET_OT_addApproximateEmpty.bl_idname)
+        col.operator(DDDET_OT_convertEmptyAndSphere.bl_idname)
+
 ################################################################
 def menu_fn(self, context):
     layout = self.layout
     layout.separator()
-    layout.operator(DDDST_OT_selectDividingLoops.bl_idname)
+    layout.operator(DDDET_OT_selectDividingLoops.bl_idname)
 
 ################################################################
 classes = (
-    DDDST_OT_selectDividingLoops,
-    DDDST_PT_SelectTool,
+    DDDET_OT_selectDividingLoops,
+    DDDET_OT_addApproximateSphere,
+    DDDET_OT_addApproximateEmpty,
+    DDDET_OT_convertEmptyAndSphere,
+    DDDET_PT_main,
 )
 
 def registerClass():
