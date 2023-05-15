c8b54fcf8493eb79bd7084dc5e8aac1f3a4df74e
DaDaDan3D <dadadan@cat.nifty.jp>
2023-05-15 22:02:24 +0900

    バージョンを 0.4.8 に上げる

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ bl_info = {
     "warning": "Not much error handling. Use at your own risk.",
     "support": "TESTING",
     "author": "DaDaDan3D",
-    "version": (0, 4, 6),
+    "version": (0, 4, 8),
     "blender": (3, 1, 0),
     "doc_url": "https://github.com/DaDaDan3D/DDDTools",
     "tracker_url": "https://github.com/DaDaDan3D/DDDTools/issues",
